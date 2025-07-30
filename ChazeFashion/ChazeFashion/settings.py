b047e5d7a1658dcfad38fa94852e426ebb334984
Khush Sahu <sahukhush2005@gmail.com>
2025-07-30 08:03:19 +0530

    Commit updated

--- ChazeFashion/ChazeFashion/settings.py
+++ ChazeFashion/ChazeFashion/settings.py
@@ -132,4 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+LOGIN_URL = '/login/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
